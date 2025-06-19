9be82068f33d759a53ae0404c283515ee0191425
Mohit <mohitswarnkar13@gmail.com>
2025-06-20 03:52:52 +0530

    dummy Route protection

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(passport.session());
 app.use(require("cors")({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
